64561da370290a0c673688be12e80b39c3789cc7
Rodney van den Velden <rodneymichael2002@gmail.com>
2026-05-13 13:29:44 +0200

    Fancy logging and notifications

--- src/update.ts
+++ src/update.ts
@@ -1,8 +1,12 @@
-import { build, checkUpdateNeeded, clean, clone, parsePkgList } from './util.ts';
+import { build, checkUpdateNeeded, clean, clone, logSummary, parsePkgList, sendNotification } from './util.ts';
 
 const pkglist = parsePkgList('pkglist');
 
 async function updateAur() {
+  const success: string[] = [];
+  const failed: string[] = [];
+  const skipped: string[] = [];
+
   for (const pkg of pkglist) {
     await clone(pkg);
 
@@ -10,18 +14,25 @@ async function updateAur() {
       const buildStatus = await build(`aur/${pkg}`);
 
       if (buildStatus) {
-        console.log(`${pkg} has succesfully finished building.`);
+        console.log(`${pkg} has successfully finished building.`);
+        Deno.copyFileSync(`aur/${pkg}/PKGBUILD`, `cache/${pkg}.pkgbuild`);
+        await sendNotification('Nightly Builder', `${pkg} has been built successfully.`)
+        success.push(pkg);
       } else {
         console.log(`${pkg} has failed to build.`);
+        await sendNotification('Nightly Builder', `${pkg} has failed building.`)
+        failed.push(pkg);
       }
 
-      Deno.copyFileSync(`aur/${pkg}/PKGBUILD`, `cache/${pkg}.pkgbuild`);
     } else {
       console.log(`Skipping ${pkg}, no update needed.`);
+      skipped.push(pkg);
     }
   }
-  
+
   clean();
+  logSummary(success, failed, skipped);
+  await sendNotification('Nightly Builder', 'Nightly Builder has finished.');
 }
 
 await updateAur();
--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,7 @@
 import { git } from '@roka/git';
+import { green, red, yellow } from '@std/fmt/colors';
 import { existsSync } from 'node:fs';
+import { config } from "../config.ts";
 
 export function parsePkgList(file: string): string[] {
   return Deno.readTextFileSync(file).trim().split('\n');
@@ -18,7 +20,7 @@ export async function build(path: string): Promise<boolean> {
 
   const buildCommand = new Deno.Command(`/usr/bin/makepkg`, {
     args: [
-      '-s', '--clean', '--config', `${cwd}/makepkg.conf`
+      '-s', '-f', '--clean', '--config', `${cwd}/makepkg.conf`
     ]
   });
 
@@ -44,4 +46,42 @@ export function checkUpdateNeeded(pkg: string): boolean {
   const newPkgbuild = Deno.readTextFileSync(`aur/${pkg}/PKGBUILD`);
 
   return cachedPkgbuild !== newPkgbuild;
+}
+
+export function logSummary(success: string[], failed: string[], skipped: string[]) {
+  if (success.length !== 0) {
+    console.log(green('\n\nSuccess'));
+    for (const pkg of success) {
+      console.log(`  - ${pkg}`);
+    }
+  }
+  
+  if (failed.length !== 0) {
+    console.log(red('\nFailed:'));
+    for (const pkg of failed) {
+      console.log(`  - ${pkg}`);
+    }
+  }
+
+  if (skipped.length !== 0) {
+    console.log(yellow('\nSkipped:'));
+    
+    for (const pkg of skipped) {
+      console.log(`  - ${pkg}`);
+    }
+  }
+}
+
+export async function sendNotification(title: string, message: string, priority: number = 5) {
+  await fetch(`${config.gotify_url}/message?token=${config.gotify_token}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      title: title,
+      message: message,
+      priority: priority
+    })
+  });
 }
